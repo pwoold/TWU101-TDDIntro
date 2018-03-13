51aa1b8d38652abd34dfa5dea162b844a599d3c2
Phoebe Wooldridge <pwooldri@c02vt3zuhtd5.local>
2018-03-13 11:22:30 -0500

    account exercise - first two tests passing; refactored to include setup method to create account instance

--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,23 +1,27 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class AccountTests {
+    private Account account;
+    @Before
+    public void setUp() {
+        account = new Account();
+        account.setBalance(100);
+    }
+
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
-        account.setBalance(100);
         account.deposit(50);
         assertThat(account.getBalance(), is(150));
     }
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-        Account account = new Account();
-        account.setBalance(100);
         account.withdraw(50);
         assertThat(account.getBalance(), is(50));
     }
