f685a45ac16d9ae8f9884880086c7c0ff9991ade
Phoebe Wooldridge <pwooldri@c02vt3zuhtd5.local>
2018-03-13 11:15:58 -0500

    committing account balanceexercise - first test passing

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+
+    private int balance;
+
+    public void setBalance(int newBalance) {
+        balance = newBalance;
+    }
+
+    public void deposit(int i) {
+        balance = balance + i;
+    }
+    public int getBalance() {
+        return balance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -2,12 +2,16 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account();
+        account.setBalance(100);
+        account.deposit(50);
+        assertThat(account.getBalance(), is(150));
     }
 
     @Test
