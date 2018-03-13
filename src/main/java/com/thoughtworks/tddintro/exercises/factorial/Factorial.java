4d3114a7df65efc8a4a6d26983a5d57485943050
Phoebe Wooldridge <pwooldri@c02vt3zuhtd5.local>
2018-03-13 11:05:33 -0500

    committing factorial test exercise - all tests passing

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        if (i < 2) {
+    public Integer compute(int i) throws IllegalArgumentException {
+        if (i<0) {
+            throw new IllegalArgumentException();
+        }
+        else if (i < 2) {
             return 1;
         }
         return i*compute(i-1);
